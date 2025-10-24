c299b6ce1c71da64c2f6492d73f264eec856abc0
Arthur Menegoli <arthur.menegoli@gmail.com>
2025-10-24 08:40:45 -0300

    Remove logs and clean observers

--- Observer/ApplyCouponObserver.php
+++ Observer/ApplyCouponObserver.php
@@ -11,24 +11,14 @@ class ApplyCouponObserver implements ObserverInterface
     {
 		$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 		$logger = $objectManager->get(\Psr\Log\LoggerInterface::class);
-//		$cartHelper = $objectManager->get(\Crankycyclops\DiscountCodeUrl\Helper\Cart::class);
 		$cookieHelper = $objectManager->get(\Crankycyclops\DiscountCodeUrl\Helper\Cookie::class);
 		$config = $objectManager->get(\Crankycyclops\DiscountCodeUrl\Helper\Config::class);
-//		$couponModel = $objectManager->get(\Magento\SalesRule\Model\Coupon::class);
-//		$ruleModel = $objectManager->get(\Magento\SalesRule\Model\Rule::class);
 		$registry = $objectManager->get(\Magento\Framework\Registry::class);
-//		$checkoutSession = $objectManager->get(\Magento\Checkout\Model\Session::class);
-
-
         $request = $observer->getEvent()->getRequest();
         $uri = $request->getRequestUri();
-
-//        $logger->info('[Crankycyclops - Coupon URL] - uri: ' . $uri);
-
         if (strpos($uri, '/page_cache/') !== false) {
             return;
         }
-
         $queryParameter = $config->getUrlParameter();
         $couponCode = $request->getParam($queryParameter);
         if ($couponCode) {
--- Plugin/ApplyCouponOnAddToCart.php
+++ Plugin/ApplyCouponOnAddToCart.php
@@ -30,15 +30,12 @@ class ApplyCouponOnAddToCart
     ) {
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $logger = $objectManager->get(\Psr\Log\LoggerInterface::class);
-        $logger->info('[Crankycyclops - Coupon URL] - Iniciou o plugin de add to cart.');
         $coupon = $this->registry->registry('crankycyclops_discounturl_coupon_noquote');
         $cookieHelper = $objectManager->get(\Crankycyclops\DiscountCodeUrl\Helper\Cookie::class);
     	$coupon = $cookieHelper->getCookie();
-        $logger->info('[Crankycyclops - Coupon URL] - coupon: ' . $coupon);
         if (!empty($coupon)) {
             $quote = $this->checkoutSession->getQuote();
             if (!$quote->getCouponCode()) {
-                $logger->info('[Crankycyclops - Coupon URL] - entrou no if tudo.');
                 $this->cartHelper->applyCoupon($quote, $coupon);
             }
             $this->registry->unregister('crankycyclops_discounturl_coupon_noquote');
